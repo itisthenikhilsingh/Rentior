bb4a67478547ae6dead5d0c7bee507f463ca51e6
itisthenikhilsingh <itisthenikhilsingh@gmail.com>
2024-08-07 16:50:06 +0530

    initial backed setup

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+const app = express();
+//to make input as json
+app.use(express.json());
+app.use(cors());
+app.use(express.static("public"));
+
+app.listen(3000, () => {
+  console.log("server is running on port 3000");
+});
