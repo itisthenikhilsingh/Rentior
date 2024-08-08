ef7880fe077be7a6bb656d24f641eb7c471412f1
itisthenikhilsingh <itisthenikhilsingh@gmail.com>
2024-08-09 00:07:26 +0530

    coorecting the model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -20,7 +20,7 @@ const userSchema = new mongoose.Schema(
       required: true,
       unique: true,
     },
-    profileImage: {
+    profileImagePath: {
       type: String,
       default: "",
     },
